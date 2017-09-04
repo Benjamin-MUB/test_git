e0556c892e7cbef9120527e534c6a9d52903d6af
Benjamin Chikhoune <benjamin.chickoune@matchupbox.com>
2017-09-04 14:09:07 +0200

    maj

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+
 int            main()
 {
    write(1, "OK", strlen("OK"));
+   write(1, "\n", 2);
+   printf("FIN\n");
+   
    return (0);
 }
